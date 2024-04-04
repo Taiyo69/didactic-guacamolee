4e2a8c2023293e594ce73453b0c63dfee1cb908b
Taiyo69 <mitsuyaok@gmail.com>
2024-04-04 11:32:23 -0400

    update

--- express.js
+++ express.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 const PORT = process.env.PORT ?? 1234
-const moviesJSON = require('./movies.json')
+const movies = require('./movies.json')
 
 app.disable('x-powered-by')
 
@@ -12,13 +12,20 @@ app.get('/', (req, res) => {
 })
 
 app.get('/movies', (req, res) => {
-    res.json(moviesJSON)
+    const { genre } = req.query
+    if (genre) {
+        const filteredMovies = movies.filter(movie => movie.genre.some(g => g.toLowerCase() === genre.toLowerCase()))
+        return res.json(filteredMovies)
+    }
+    res.json(movies)
 })
 
 app.get('/movies/:id', (req, res) => {
     const { id } = req.params
+    const movie = movies.find(movie => movie.id === id)
+    if(movie) return res.json(movie)
 
-
+    res.status(404).json({ message: 'Movie not found' })
 })
 
 app.listen(PORT, () => {
