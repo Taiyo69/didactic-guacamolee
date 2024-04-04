58d9b8a348803ba8d0a934008f4eafbbc9be4425
Taiyo69 <mitsuyaok@gmail.com>
2024-04-04 11:03:54 -0400

    update

--- express.js
+++ express.js
@@ -2,10 +2,23 @@ const express = require('express')
 const app = express()
 
 const PORT = process.env.PORT ?? 1234
+const moviesJSON = require('./movies.json')
+
+app.disable('x-powered-by')
 
 app.get('/', (req, res) => {
     console.log('request received')
     res.send('Hola mundo')
+})
+
+app.get('/movies', (req, res) => {
+    res.json(moviesJSON)
+})
+
+app.get('/movies/:id', (req, res) => {
+    const { id } = req.params
+
+
 })
 
 app.listen(PORT, () => {
